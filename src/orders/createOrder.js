5a0440ef0e726bf224dc52277629ca183ffe8e99
Lucas Dias <lucasrafa2008@gmail.com>
2025-04-08 22:18:01 -0300

    Initialize serverless project with basic structure and  functions

---
+++ src/orders/createOrder.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function createOrder() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default createOrder
---
+++ src/orders/getOrders.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function getOrders() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default getOrders
